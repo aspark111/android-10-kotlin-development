4905be8a050f41365fbeed4335631615eb62dae6
Alex <alexp@bioreigns.com>
2021-10-15 15:25:08 -0700

    Button functionality

--- app/src/main/java/eu/tutorials/helloworld/MainActivity.kt
+++ app/src/main/java/eu/tutorials/helloworld/MainActivity.kt
@@ -2,10 +2,17 @@ package eu.tutorials.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_click_me = findViewById<Button>(R.id.button)
+        btn_click_me.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me", Toast.LENGTH_SHORT).show()
+        }
     }
 }
